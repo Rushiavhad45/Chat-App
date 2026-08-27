47631ef8437091cf4ec59e65334a174fecb119bf
rushikesh <rushikeshavhad45@gmail.com>
2026-08-27 19:35:11 +0530

    login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+   <Routes>
+    <Route path="/" element={<Chatpage />} />
+    <Route path="/login" element={<NavigatorLoginpage />} />
+    <Route path="/signup" element={<Signuppage />} />
+   </Routes>
   )
 }
 
