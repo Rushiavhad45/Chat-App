b4a0e0d6b2bf49b80a535f663d53195398d2ec94
rushikesh <rushikeshavhad45@gmail.com>
2026-08-27 20:03:07 +0530

    n

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import { Routes, Route } from 'react-router'
+import Chatpage from './pages/Chatpage'
+import Signuppage from './pages/Signuppage'
+import Loginpage from './pages/Loginpage'
+import { useAuthStore } from './store/authStore'
 
 function App() {
+  const { authUser, isloading, login } = useAuthStore();
+  console.log("auth user", authUser);
+  console.log("isloading", isloading);
+
+  
   return (
-   <Routes>
-    <Route path="/" element={<Chatpage />} />
-    <Route path="/login" element={<NavigatorLoginpage />} />
-    <Route path="/signup" element={<Signuppage />} />
-   </Routes>
+    <div className ="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
+
+    <Routes>
+     <Route path="/" element={<Chatpage />} />
+     <Route path="/login" element={<Loginpage />} />
+     <Route path="/signup" element={<Signuppage />} />
+    </Routes>
+   </div>
   )
 }
 
