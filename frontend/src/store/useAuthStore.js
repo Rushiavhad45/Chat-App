c7e0f6fb8488e02ca5ae10527c62ac2a45dd4939
rushikesh <rushikeshavhad45@gmail.com>
2026-08-27 20:17:09 +0530

    zustand

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,10 +3,11 @@ import {create} from 'zustand'
 export const useAuthStore = create((set) => ({ 
     authUser : {name : "Rushikesh", _id :123, age: 22 },
     isLoggedIn : false,
+    isLoading : false,
 
     login : () => {
         console.log("We just logged in");
-        set({ isLoggedIn : true});
+        set({ isLoggedIn : true, isLoading:true });
     }
 }));
 
