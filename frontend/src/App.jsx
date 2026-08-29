1224e8d2b70fdb69b2ef36d82b2d617e4bdbd909
rushikesh <rushikeshavhad45@gmail.com>
2026-08-29 14:35:44 +0530

    Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,24 +3,35 @@ import Chatpage from './pages/Chatpage'
 import Signuppage from './pages/Signuppage'
 import Loginpage from './pages/Loginpage'
 import { useAuthStore } from './store/authStore'
+import { useEffect } from "react";
+import PageLoader from "./components/PageLoader";
+
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  const { authUser, isloading, login } = useAuthStore();
-  console.log("auth user", authUser);
-  console.log("isloading", isloading);
+  const { checkAuth, isCheckingAuth, authUser } = useAuthStore();
 
-  
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth) return <PageLoader />;
+
   return (
-    <div className ="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
+    <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
+      {/* DECORATORS - GRID BG & GLOW SHAPES */}
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
+      <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
+      <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
 
-    <Routes>
-     <Route path="/" element={<Chatpage />} />
-     <Route path="/login" element={<Loginpage />} />
-     <Route path="/signup" element={<Signuppage />} />
-    </Routes>
-   </div>
-  )
+      <Routes>
+        <Route path="/" element={authUser ? <ChatPage /> : <Navigate to={"/login"} />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to={"/"} />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />} />
+      </Routes>
+
+      <Toaster />
+    </div>
+  );
 }
-
-export default App
-
+export default App;
