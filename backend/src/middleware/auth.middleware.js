a710a4bf8ca3e215f0395a5068cf5793ffb4f8bf
rushikesh <rushikeshavhad45@gmail.com>
2026-09-03 14:49:27 +0530

    useronline

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -20,3 +20,5 @@ export const protectRoute = async (req, res, next) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+
--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// make ready for deployment
+// make ready 
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
